f3914718ffee8bce326b685a40f41b852cd671a4
Kuchiwo <competor@gmail.com>
2021-11-02 11:47:01 +0900

    52

--- docs/main.js
+++ docs/main.js
@@ -21,7 +21,7 @@ const LENGTH_PER_THREAD = Math.ceil(characters.length / MAX_THREADS);
 
 // 処理中...と表示
 const indicator = document.createElement('div');
-indicator.innerText = '処理中...';
+indicator.innerText = '52';
 document.body.appendChild(indicator);
 
 
